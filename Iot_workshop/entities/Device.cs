c866b6955d885cca19b776e123e43e6999f3fde8
Tomer <tomehtomeh@gmail.com>
2016-04-21 18:10:20 +0300

    inserted xamarin project to the solution

---
+++ Iot_workshop/entities/Device.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iot_workshop.entities
+{
+    class Device
+    {
+        [JsonProperty(PropertyName = "Id")]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "name")]
+        public string name { get; set; }
+
+        [JsonProperty(PropertyName = "location")]
+        public string location { get; set; }
+    }
+}
